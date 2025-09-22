e418738d8091cd9f0ce6f4e7092be0b03f56fe2f
ada <adelya030720@gmail.com>
2025-09-22 23:42:18 +0300

    сделала модели

--- PrjRecept/settings.py
+++ PrjRecept/settings.py
@@ -1,17 +1,11 @@
 
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ar)m*x5$qlg1#=+madjm0lu5qzflnl5gn6lfbvzjvzcg9c0m@q'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -61,8 +55,6 @@ WSGI_APPLICATION = 'PrjRecept.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -72,8 +64,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -91,7 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
 
@@ -101,13 +90,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- recept/models.py
+++ recept/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your models here.
+class User(models.Model):
+     name = models.CharField(max_length=150,
+                            help_text='имя пользователя',
+                            verbose_name='Введителя имя пользователя')
+     email = models.EmailField(unique=True)
+     phone_num = models.CharField(max_length=20, blank=True, null=True)
+     password_hash = models.CharField(max_length=128)
+     created_at = models.DateTimeField(auto_now_add=True)
+
+     def set_password(self, raw_password):
+        self.password_hash = make_password(raw_password)
+
+     def check_password(self, raw_password):
+        return check_password(raw_password, self.password_hash)
+
+     def __str__(self):
+        return self.name
+     
+
+class MealType(models.TextChoices):
+    BREAKFAST = 'breakfast', 'Завтрак'
+    LUNCH = 'lunch', 'Обед'
+    DINNER = 'dinner', 'Ужин'
+
+
+class MainIngredient(models.Model):
+    UNIT_CHOICES = [
+        ('g', 'Граммы'),
+        ('ml', 'Миллилитры'),
+        ('pcs', 'Штуки'),
+    ]
+
+    name = models.CharField(max_length=50, unique=True)
+    unit = models.CharField(max_length=5, choices=UNIT_CHOICES, default='pcs', help_text='Единица измерения')
+
+    def __str__(self):
+        return self.name
+    
+
+class Recipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    title = models.CharField(max_length=200)
+    cover_image = models.ImageField(upload_to='recipe_images/', blank=True, null=True, help_text='Обложка рецепта')
+    description = models.TextField()
+    portions = models.PositiveIntegerField(default=1)
+    calories = models.PositiveIntegerField(help_text='Калорийность на порцию')
+    estimated_cost = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    meal_type = models.CharField(max_length=20, choices=MealType.choices)
+    main_ingredients = models.ManyToManyField(MainIngredient, related_name='recipes')
+    video_file = models.FileField(upload_to='recipe_videos/', blank=True, null=True, help_text='Видео рецепт (файл)')
+    is_public = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class RecipeStep(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='steps')
+    order = models.PositiveIntegerField(help_text='Порядок шага')
+    description = models.TextField(blank=True, null=True, help_text='Текстовое описание шага')
+    image = models.ImageField(upload_to='recipe_steps/', blank=True, null=True, help_text='Картинка к шагу')
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f'{self.recipe.title} - шаг {self.order}'
+    
+    
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredients')
+    ingredient = models.ForeignKey(MainIngredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=6, decimal_places=2, help_text='Количество ингредиента')
+
+    def __str__(self):
+        return f'{self.quantity} {self.ingredient.get_unit_display()} {self.ingredient.name}'   
+
+class Review(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    rating = models.PositiveSmallIntegerField(default=0)  # рейтинг 0-5
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('recipe', 'user')
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='favorited_by')
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
